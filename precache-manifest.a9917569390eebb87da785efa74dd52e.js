f64624a88a56a6a0c5aa00cd9245ddb622c6f09c
Deepanshu Yadav <daybond007@gmail.com>
2019-05-07 05:18:56 +0530

    Updates

---
+++ precache-manifest.a9917569390eebb87da785efa74dd52e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac0175384f89899c397116440789449",
+    "url": "/TwistYoFate/Portfolio/static/media/pic.dac01753.jpg"
+  },
+  {
+    "revision": "4db3a08bc86fc57fdfe6ad43fd558473",
+    "url": "/TwistYoFate/Portfolio/static/media/ml.4db3a08b.png"
+  },
+  {
+    "revision": "7e03175d4390d6b0435a3d514fd0edb8",
+    "url": "/TwistYoFate/Portfolio/static/media/gh.7e03175d.png"
+  },
+  {
+    "revision": "8134798c9daea34ba0c2222ca3124bde",
+    "url": "/TwistYoFate/Portfolio/static/media/ct.8134798c.png"
+  },
+  {
+    "revision": "889899ad04789fe334b7",
+    "url": "/TwistYoFate/Portfolio/static/js/runtime~main.889899ad.js"
+  },
+  {
+    "revision": "a487e52a9f39db7b5311",
+    "url": "/TwistYoFate/Portfolio/static/js/main.a487e52a.chunk.js"
+  },
+  {
+    "revision": "a4121f5f4679112d0b9a",
+    "url": "/TwistYoFate/Portfolio/static/js/2.a4121f5f.chunk.js"
+  },
+  {
+    "revision": "a487e52a9f39db7b5311",
+    "url": "/TwistYoFate/Portfolio/static/css/main.3ca5754a.chunk.css"
+  },
+  {
+    "revision": "a4121f5f4679112d0b9a",
+    "url": "/TwistYoFate/Portfolio/static/css/2.1756418c.chunk.css"
+  },
+  {
+    "revision": "eb3481c27a8b16bacba80f0646d4e05b",
+    "url": "/TwistYoFate/Portfolio/index.html"
+  }
+];
